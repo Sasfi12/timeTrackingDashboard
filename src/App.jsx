2a6401196750d5a2fb10d0e6e9dd59a5285786cb
Sasfi12 <sufyan10.muhammad1070@hotmail.com>
2025-02-24 19:57:24 +0100

    added formatting and planning automation of bgColors

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,45 @@ import './App.css'
 import User from './components/user/User'
 import Timeframe from './components/timeframe/Timeframe'
 import data from "../public/data.json"
+import social from "../public/images/icon-social.svg"
+import exercise from "../public/images/icon-exercise.svg"
+import play from "../public/images/icon-play.svg"
+import selfCare from "../public/images/icon-self-care.svg"
+import study from "../public/images/icon-study.svg"
+import work from "../public/images/icon-work.svg"
+
 function App() {
   const [currentlySelected , setCurrentlySelected] = useState("monthly") ;
-
+  const images = [work, play , study , exercise , social , selfCare]
+  const bgColors = []
   const MapTimeFrames = (
       data.map(function(e , index) {
         if(currentlySelected == "monthly")
           return (
-            <Timeframe key={index} title={e.title} current= {e.timeframes.monthly.current} previous= {e.timeframes.monthly.previous} />
+            <Timeframe 
+              key={index} 
+              title={e.title} 
+              current= {e.timeframes.monthly.current} 
+              previous= {e.timeframes.monthly.previous} 
+              image= {images[index]} />
             )
         else if(currentlySelected == "daily")
           return (
-            <Timeframe key={index} title={e.title} current= {e.timeframes.daily.current} previous= {e.timeframes.daily.previous} />
+            <Timeframe 
+              key={index} 
+              title={e.title} 
+              current= {e.timeframes.daily.current} 
+              previous= {e.timeframes.daily.previous} 
+              image= {images[index]} />
             )
         else if(currentlySelected == "weekly")
           return (
-            <Timeframe key={index} title={e.title} current= {e.timeframes.weekly.current} previous= {e.timeframes.weekly.previous} />
+            <Timeframe 
+              key={index} 
+              title={e.title} 
+              current= {e.timeframes.weekly.current} 
+              previous= {e.timeframes.weekly.previous} 
+              image= {images[index]} />
             )
       })
     )
